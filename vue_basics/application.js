f7cce7ececc44219de1c324747608cf12baa85d4
Shahbaz <shahbaz_ali@outlook.in>
2023-01-26 17:09:10 +0530

    make a basic quiz app

--- vue_basics/application.js
+++ vue_basics/application.js
@@ -1,3 +1,35 @@
-let message = "Hello World";
+const questions = [
+    "What is your name?",
+    "What would you rather be doing?",
+    "What is your preferred programming language?",
+    "What is your favorite food?",
+    "What is your favorite color?",
+    "What is your favorite animal?",
+    "What is your favorite movie?"
+];
 
-document.getElementById("app").innerHTML("HelloWorld");
+var app = new Vue({
+    el : "#app",
+    data : {
+        noOfQns: questions.length,
+        question : questions[0],
+        prompt: "Please Reply ...",
+        count : 0,
+        responses: []
+    },
+    methods: {
+        handleReply: function() {
+            this.responses.push({question: this.question, answer: this.reply});
+            this.count++;
+            if (this.count < this.noOfQns) {
+                this.question = questions[this.count];
+                this.prompt= "Great Keep Going!";
+                this.reply = "";
+            } else {
+                this.question = "That's all !"
+                this.prompt = "Thank you for your time! Here are your responses:";
+                this.reply = "";
+            }
+        }
+    }
+})
